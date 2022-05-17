a4c6632c717e9913fcab400d88cf5f77d6fe6077
VArtzy <farrelnikoson@gmail.com>
2022-05-17 14:15:45 +0700

    tambah website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    fontFamily: {
+      'quicksand': ['Quicksand'],
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl' : '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
